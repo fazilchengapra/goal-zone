999702339d86cf2862be0265b151c07d1975c414
Fazil Chengapra <muhammedfazilchengapra@gmail.com>
2025-03-29 20:59:52 +0530

    table row data design added

--- app/MatchDetails.tsx
+++ app/MatchDetails.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { CiCircleInfo } from "react-icons/ci";
+import { BsGraphUp } from "react-icons/bs";
+
 
 const MatchDetials = () => {
   return (
@@ -18,7 +21,7 @@ const MatchDetials = () => {
             name="my_tabs_2"
             className="tab before:text-[#5742A9]"
             aria-label={e}
-            defaultChecked={ e === 'Latest Match'}
+            defaultChecked={e === "Latest Match"}
           />
         ))}
       </div>
@@ -26,9 +29,53 @@ const MatchDetials = () => {
         <tbody>
           {/* row 1 */}
           <tr>
-            <td>Cy Ganderton</td>
-            <td>Quality Control Specialist</td>
-            <td>Blue</td>
+            <td>
+              <div className="flex gap-3 items-center">
+                <div className="avatar">
+                  <div className="w-8 rounded-full">
+                    <img
+                      src="https://r2.thesportsdb.com/images/media/team/logo/r7pn8c1726167467.png"
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <div className="text-sm">Argentina</div>
+              </div>
+            </td>
+            <td>
+              <div className="flex flex-col justify-center items-center">
+                <div className="bg-[#E6E6E6] w-fit px-4 text-sm rounded-2xl font-semibold text-[#5742A9]">
+                  2 - 2
+                </div>
+              </div>
+            </td>
+            <td>
+              <div className="flex gap-3 items-center">
+                <div className="avatar">
+                  <div className="w-8 rounded-full">
+                    <img
+                      src="https://r2.thesportsdb.com/images/media/team/badge/fxijcp1726167035.png"
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <div className="text-sm">Italy</div>
+              </div>
+            </td>
+            <td>
+              <div className="badge bg-[#FBE3E3] text-sm font-light text-[#FA0E0E] px-3 py-4">
+                Full - Time
+              </div>
+            </td>
+            <td>
+              <p className="text-[#636363]">18 December 2022</p>
+            </td>
+            <td>
+              <div className="flex flex-row gap-4 items-center">
+                <CiCircleInfo size={25} color="#636363"/>
+                <BsGraphUp size={25} color="#636363"/>
+              </div>
+            </td>
           </tr>
           {/* row 2 */}
           <tr>

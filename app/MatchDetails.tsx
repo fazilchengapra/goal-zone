7740433e2fb4c30ed4626ddf6459e199913fc428
Fazil Chengapra <muhammedfazilchengapra@gmail.com>
2025-03-29 20:18:55 +0530

    table tab added

--- app/MatchDetails.tsx
+++ app/MatchDetails.tsx
@@ -3,34 +3,41 @@ import React from "react";
 const MatchDetials = () => {
   return (
     <div className="w-full overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+      <p className="mt-5 mx-5 text-md font-semibold">⚽ Football Match</p>
+      <div className="tabs tabs-border my-5 ml-3 tabs-md flex flex-row flex-nowrap">
+        {[
+          "Latest Match",
+          "Coming Match",
+          "Pre-season",
+          "Live Games",
+          "Fun Football",
+        ].map((e) => (
+          <input
+            key={e}
+            type="radio"
+            name="my_tabs_2"
+            className="tab before:text-[#5742A9]"
+            aria-label={e}
+            defaultChecked={ e === 'Latest Match'}
+          />
+        ))}
+      </div>
       <table className="table">
-        {/* head */}
-        <thead>
-          <tr>
-            <th></th>
-            <th>Name</th>
-            <th>Job</th>
-            <th>Favorite Color</th>
-          </tr>
-        </thead>
         <tbody>
           {/* row 1 */}
           <tr>
-            <th>1</th>
             <td>Cy Ganderton</td>
             <td>Quality Control Specialist</td>
             <td>Blue</td>
           </tr>
           {/* row 2 */}
           <tr>
-            <th>2</th>
             <td>Hart Hagerty</td>
             <td>Desktop Support Technician</td>
             <td>Purple</td>
           </tr>
           {/* row 3 */}
           <tr>
-            <th>3</th>
             <td>Brice Swyre</td>
             <td>Tax Accountant</td>
             <td>Red</td>
--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
             <Sidebar />
             <div className="flex flex-col flex-1 h-screen">
               <Navbar />
-              <div className="flex-1 overflow-y-auto w-11/12 m-auto">{children}</div>
+              <div className="flex-1 overflow-y-auto w-11/12 m-auto my-5">{children}</div>
             </div>
           </div>
         </ReduxProvider>

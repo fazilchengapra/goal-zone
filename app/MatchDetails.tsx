264a0118ea7f7a2ddbafb5d0f1318db1056809cb
Fazil Chengapra <muhammedfazilchengapra@gmail.com>
2025-04-02 17:15:36 +0530

    ⚽ Football Match dummy data added

--- app/MatchDetails.tsx
+++ app/MatchDetails.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { CiCircleInfo } from "react-icons/ci";
 import { BsGraphUp } from "react-icons/bs";
+import { match } from "@/public/assets/constant/match";
 
 const MatchDetials = () => {
   return (
@@ -26,71 +27,60 @@ const MatchDetials = () => {
       </div>
       <table className="table">
         <tbody>
-          {/* row 1 */}
-          <tr>
-            <td>
-              <div className="flex gap-3 items-center">
-                <div className="avatar">
-                  <div className="w-8 rounded-full">
-                    <img
-                      src="https://r2.thesportsdb.com/images/media/team/logo/r7pn8c1726167467.png"
-                      alt=""
-                    />
+          {match.map((e) => (
+            <tr key={e.date + e.team1.name+ e.team1.score}>
+              <td>
+                <div className="flex gap-3 items-center">
+                  <div className="avatar">
+                    <div className="w-8 rounded-full">
+                      <img
+                        src={e.team1.logo}
+                        alt="logo"
+                      />
+                    </div>
+                  </div>
+                  <div className="text-sm">{e.team1.name}</div>
+                </div>
+              </td>
+              <td>
+                <div className="flex flex-col justify-center items-center">
+                  <div className="bg-[#EFEDF7] w-fit px-4 text-sm rounded-2xl font-semibold text-[#5742A9]">
+                    {`${e.team1.score} - ${e.team2.score}`}
                   </div>
                 </div>
-                <div className="text-sm">Argentina</div>
-              </div>
-            </td>
-            <td>
-              <div className="flex flex-col justify-center items-center">
-                <div className="bg-[#EFEDF7] w-fit px-4 text-sm rounded-2xl font-semibold text-[#5742A9]">
-                  2 - 2
+              </td>
+              <td>
+                <div className="flex gap-3 items-center">
+                  <div className="avatar">
+                    <div className="w-8 rounded-full">
+                      <img
+                        src={e.team2.logo}
+                        alt='logo'
+                      />
+                    </div>
+                  </div>
+                  <div className="text-sm">{e.team2.name}</div>
                 </div>
-              </div>
-            </td>
-            <td>
-              <div className="flex gap-3 items-center">
-                <div className="avatar">
-                  <div className="w-8 rounded-full">
-                    <img
-                      src="https://r2.thesportsdb.com/images/media/team/badge/fxijcp1726167035.png"
-                      alt=""
-                    />
+              </td>
+              <td>
+                <div className="flex flex-row items-center">
+                  <div className="w-[6rem] md:w-[7rem] badge bg-red-100 text-xs md:text-sm font-light text-red-600 px-2 py-1 h-fit lg:px-4 lg:py-2">
+                    <p className=" min-w-auto">{e.statusText}</p>
                   </div>
                 </div>
-                <div className="text-sm">Italy</div>
-              </div>
-            </td>
-            <td>
-              <div className="flex flex-row items-center">
-                <div className="w-[6rem] md:w-[7rem] badge bg-red-100 text-xs md:text-sm font-light text-red-600 px-2 py-1 h-fit lg:px-4 lg:py-2">
-                  <p className=" min-w-auto">Full - Time</p>
-                </div>
-              </div>
-            </td>
+              </td>
 
-            <td>
-              <p className="text-[#636363]">18 December 2022</p>
-            </td>
-            <td>
-              <div className="flex flex-row gap-4 items-center">
-                <CiCircleInfo size={25} color="#636363" />
-                <BsGraphUp size={25} color="#636363" />
-              </div>
-            </td>
-          </tr>
-          {/* row 2 */}
-          <tr>
-            <td>Hart Hagerty</td>
-            <td>Desktop Support Technician</td>
-            <td>Purple</td>
-          </tr>
-          {/* row 3 */}
-          <tr>
-            <td>Brice Swyre</td>
-            <td>Tax Accountant</td>
-            <td>Red</td>
-          </tr>
+              <td>
+                <p className="text-[#636363]">18 December 2022</p>
+              </td>
+              <td>
+                <div className="flex flex-row gap-4 items-center">
+                  <CiCircleInfo size={25} color="#636363" />
+                  <BsGraphUp size={25} color="#636363" />
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
---
+++ public/assets/constant/match.tsx
@@ -0,0 +1,82 @@
+export const match = [
+  {
+    date: "2023-10-01",
+    team1: {
+      name: "Argentina",
+      score: 1,
+      logo: "https://r2.thesportsdb.com/images/media/team/logo/r7pn8c1726167467.png",
+    },
+    team2: {
+      name: "Italy",
+      score: 2,
+      logo: "https://r2.thesportsdb.com/images/media/team/badge/fxijcp1726167035.png",
+    },
+    status: "full-time",
+    statusText: "Full - Time",
+  },
+  {
+    date: "2023-10-01",
+    team1: {
+      name: "Portugal",
+      score: 2,
+      logo: "https://r2.thesportsdb.com/images/media/team/logo/4jwa1i1599314931.png",
+    },
+    team2: {
+      name: "Belgium",
+      score: 3,
+      logo: "https://r2.thesportsdb.com/images/media/team/fanart/4y8o2n1727329982.jpg",
+    },
+    status: "full-time",
+    statusText: "Full - Time",
+  },
+
+  {
+    date: "2023-10-01",
+    team1: {
+      name: "Ghana",
+      score: 1,
+      logo: "https://flagcdn.com/gh.svg",
+    },
+    team2: {
+      name: "Belgium",
+      score: 3,
+      logo: "https://flagcdn.com/br.svg",
+    },
+    status: "full-time",
+    statusText: "Full - Time",
+  },
+
+  {
+    date: "2023-10-01",
+    team1: {
+      name: "Uruguay",
+      score: 2,
+      logo: "https://flagcdn.com/uy.svg",
+    },
+    team2: {
+      name: "Poland",
+      score: 2,
+      logo: "https://flagcdn.com/pl.svg",
+    },
+    status: "full-time",
+    statusText: "Full - Time",
+  },
+
+  {
+    date: "2023-10-01",
+    team1: {
+      name: "Spain",
+      score: 3,
+      logo: "https://flagcdn.com/es.svg",
+    },
+    team2: {
+      name: "Czech",
+      score: 3,
+      logo: "https://flagcdn.com/cz.svg",
+    },
+    status: "full-time",
+    statusText: "Full - Time",
+  }
+];
+
+
